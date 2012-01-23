99b88ebca43f35b5c504e06e792330f9725ea516
Simon Raess <cocoa@gmx.ch>
2012-01-23 22:45:43 +0100

    adding commands

---
+++ com.github.rasifix.trainings.cli/src/main/java/com/github/rasifix/trainings/shell/internal/commands/edit/ElevationCorrectionCommand.java
@@ -0,0 +1,60 @@
+package com.github.rasifix.trainings.shell.internal.commands.edit;
+
+import jline.Completor;
+import jline.NullCompletor;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+
+import com.github.rasifix.trainings.ElevationModel;
+import com.github.rasifix.trainings.model.Activity;
+import com.github.rasifix.trainings.model.Position;
+import com.github.rasifix.trainings.model.Track;
+import com.github.rasifix.trainings.model.Trackpoint;
+import com.github.rasifix.trainings.model.attr.AltitudeAttribute;
+import com.github.rasifix.trainings.model.attr.PositionAttribute;
+import com.github.rasifix.trainings.shell.Command;
+import com.github.rasifix.trainings.shell.CommandContext;
+
+@Component
+public class ElevationCorrectionCommand implements Command {
+
+	private static final String NAME = "edit:elevation";
+	
+	private ElevationModel elevationModel;
+
+	@Reference
+	public void setElevationModel(ElevationModel elevationModel) {
+		this.elevationModel = elevationModel;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public Completor getCompletor() {
+		return new NullCompletor();
+	}
+
+	@Override
+	public Object execute(CommandContext context) throws Exception {
+		Activity activity = (Activity) context.getCurrent();
+		
+		for (Track track : activity.getTracks()) {
+			for (Trackpoint trackpoint : track.getTrackpoints()) {
+				if (trackpoint.hasAttribute(PositionAttribute.class)) {
+					Position position = trackpoint.getPosition();
+					if (elevationModel.containsPosition(position.getLatitude(), position.getLongitude())) {
+						double elevation = elevationModel.elevationForPosition(position.getLatitude(), position.getLongitude());
+						trackpoint.addAttribute(new AltitudeAttribute(elevation));
+					}
+				}
+			}
+		}
+		
+		return context.getCurrent();
+	}
+
+}
--- com.github.rasifix.trainings.cli/src/main/java/com/github/rasifix/trainings/shell/internal/commands/osgi/ListServicesCommand.java
+++ com.github.rasifix.trainings.cli/src/main/java/com/github/rasifix/trainings/shell/internal/commands/osgi/ListServicesCommand.java
@@ -1,10 +1,11 @@
 package com.github.rasifix.trainings.shell.internal.commands.osgi;
 
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.ComponentContext;
-
 import jline.Completor;
 import jline.NullCompletor;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.ComponentContext;
 
 import aQute.bnd.annotation.component.Component;
 
@@ -37,7 +38,28 @@ public class ListServicesCommand implements Command {
 		String serviceClass = context.getArguments().length == 0 ? null : context.getArguments()[0];
 		ServiceReference[] references = this.context.getBundleContext().getAllServiceReferences(serviceClass, null);
 		for (ServiceReference reference : references) {
-			System.out.println(reference.getBundle().getSymbolicName() + " - " + this.context.getBundleContext().getService(reference));
+			Long serviceId = (Long) reference.getProperty("service.id");
+			String[] objectClass = (String[]) reference.getProperty("objectClass");
+			
+			System.out.println("Bundle-SymbolicName: " + reference.getBundle().getSymbolicName());
+			System.out.println("service.id         = " + serviceId);
+			for (String intf : objectClass) {
+				System.out.println("objectClass        = " + intf);
+			}
+			
+			for (String property : reference.getPropertyKeys()) {
+				if (!"service.id".equals(property) && !"objectClass".equals(property)) {
+					System.out.format("%-19s= %s\n", property, reference.getProperty(property));
+				}
+			}
+			
+			if (reference.getUsingBundles() != null) {
+				for (Bundle bundle : reference.getUsingBundles()) {
+					System.out.println("used-by            : " + bundle.getSymbolicName());
+				}
+			}
+			
+			System.out.println();
 		}
 		return context.getCurrent();
 	}
