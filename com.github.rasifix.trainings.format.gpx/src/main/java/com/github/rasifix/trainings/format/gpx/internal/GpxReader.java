3099b5e67a4235a64a0b6dcc631616b449d5e25a
Simon Raess <cocoa@gmx.ch>
2012-10-18 22:10:28 +0200

    adding support for GPX track

--- com.github.rasifix.trainings.format.gpx/src/main/java/com/github/rasifix/trainings/format/gpx/internal/GpxReader.java
+++ com.github.rasifix.trainings.format.gpx/src/main/java/com/github/rasifix/trainings/format/gpx/internal/GpxReader.java
@@ -2,6 +2,8 @@ package com.github.rasifix.trainings.format.gpx.internal;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -54,51 +56,126 @@ public class GpxReader implements ActivityReader {
 		if ("gpx".equals(root.getName())) {
 			Element rte = root.getChild("rte", NS_GPX_1_1);
 			if (rte != null) {
-				Date startTime = new Date();
-				ActivityImpl activity = new ActivityImpl(startTime);
-				
-				Position lastPosition = null;
-				double distance = 0;
-				double lastElevation = 0;
-				
-				int index = 0;
-				Track track = new Track(startTime);
-				@SuppressWarnings("unchecked")
-				List<Element> children = rte.getChildren("rtept", NS_GPX_1_1);
-				for (Element child : children) {
-					Trackpoint trackpoint = new Trackpoint(index);
-					double latitude = Double.parseDouble(child.getAttributeValue("lat"));
-					double longitude = Double.parseDouble(child.getAttributeValue("lon"));
-					double elevation = getElevationForPosition(latitude, longitude);
-					
-					Position position = new Position(latitude, longitude);
-					trackpoint.addAttribute(new PositionAttribute(position));
-					trackpoint.addAttribute(new AltitudeAttribute(elevation));
-					
-					if (lastPosition == null) {
-						trackpoint.addAttribute(new DistanceAttribute(0));
-					} else {
-						GeodeticCalculator calc = new GeodeticCalculator();
-						GlobalPosition start = new GlobalPosition(new GlobalCoordinates(lastPosition.getLatitude(), lastPosition.getLongitude()), lastElevation);
-						GlobalPosition end = new GlobalPosition(new GlobalCoordinates(latitude, longitude), elevation);
-						GeodeticMeasurement measurement = calc.calculateGeodeticMeasurement(Ellipsoid.WGS84, start, end);
-						distance += measurement.getPointToPointDistance();
-						trackpoint.addAttribute(new DistanceAttribute(distance));
-					}
-					
-					track.addTrackpoint(trackpoint);
-					
-					// get ready for next iteration
-					lastElevation = elevation;
-					lastPosition = position;
-					index += 1;
-				}
-				activity.addTrack(track);
-				result.add(activity);
+				result.add(importRoute(rte));
+			}
+			
+			Element trk = root.getChild("trk", NS_GPX_1_1);
+			if (trk != null) {
+				result.add(importTrack(trk));
 			}
 		}
 		
 		return result;
+	}
+
+	private Activity importRoute(Element rte) {
+		Date startTime = new Date();
+		ActivityImpl activity = new ActivityImpl(startTime);
+		
+		Position lastPosition = null;
+		double distance = 0;
+		double lastElevation = 0;
+		
+		int index = 0;
+		Track track = new Track(startTime);
+		@SuppressWarnings("unchecked")
+		List<Element> children = rte.getChildren("rtept", NS_GPX_1_1);
+		for (Element child : children) {
+			Trackpoint trackpoint = new Trackpoint(index);
+			double latitude = Double.parseDouble(child.getAttributeValue("lat"));
+			double longitude = Double.parseDouble(child.getAttributeValue("lon"));
+			double elevation = getElevationForPosition(latitude, longitude);
+			
+			Position position = new Position(latitude, longitude);
+			trackpoint.addAttribute(new PositionAttribute(position));
+			trackpoint.addAttribute(new AltitudeAttribute(elevation));
+			
+			if (lastPosition == null) {
+				trackpoint.addAttribute(new DistanceAttribute(0));
+			} else {
+				GeodeticCalculator calc = new GeodeticCalculator();
+				GlobalPosition start = new GlobalPosition(new GlobalCoordinates(lastPosition.getLatitude(), lastPosition.getLongitude()), lastElevation);
+				GlobalPosition end = new GlobalPosition(new GlobalCoordinates(latitude, longitude), elevation);
+				GeodeticMeasurement measurement = calc.calculateGeodeticMeasurement(Ellipsoid.WGS84, start, end);
+				distance += measurement.getPointToPointDistance();
+				trackpoint.addAttribute(new DistanceAttribute(distance));
+			}
+			
+			track.addTrackpoint(trackpoint);
+			
+			// get ready for next iteration
+			lastElevation = elevation;
+			lastPosition = position;
+			index += 1;
+		}
+		activity.addTrack(track);
+		return activity;
+	}
+
+	private Activity importTrack(Element trk) {
+		Date startTime = parseTime(trk);
+		ActivityImpl activity = new ActivityImpl(startTime);
+		
+		Position lastPosition = null;
+		double distance = 0;
+		double lastElevation = 0;
+		
+		Track track = new Track(startTime);
+		
+		Element trkseg = trk.getChild("trkseg", NS_GPX_1_1);
+		@SuppressWarnings("unchecked")
+		List<Element> trkpts = trkseg.getChildren("trkpt", NS_GPX_1_1);
+		for (Element trkpt : trkpts) {			
+			// <trkpt lat="47.015954000" lon="7.455408000"><ele>563.0</ele><time>2012-01-01T15:45:47Z</time></trkpt>
+			double latitude = Double.parseDouble(trkpt.getAttributeValue("lat"));
+			double longitude = Double.parseDouble(trkpt.getAttributeValue("lon"));
+			Position position = new Position(latitude, longitude);
+			double elevation = Double.parseDouble(trkpt.getChildTextTrim("ele", NS_GPX_1_1));
+			Date time = parseTime(trkpt);
+			
+			
+			long elapsed = time.getTime() - startTime.getTime();
+			Trackpoint trackpoint = new Trackpoint(elapsed);
+			trackpoint.addAttribute(new PositionAttribute(position));
+			trackpoint.addAttribute(new AltitudeAttribute(elevation));
+			
+			if (lastPosition == null) {
+				trackpoint.addAttribute(new DistanceAttribute(0));
+				
+			} else {
+				GeodeticCalculator calc = new GeodeticCalculator();
+				GlobalPosition start = new GlobalPosition(new GlobalCoordinates(lastPosition.getLatitude(), lastPosition.getLongitude()), lastElevation);
+				GlobalPosition end = new GlobalPosition(new GlobalCoordinates(latitude, longitude), elevation);
+				GeodeticMeasurement measurement = calc.calculateGeodeticMeasurement(Ellipsoid.WGS84, start, end);
+				distance += measurement.getPointToPointDistance();
+				trackpoint.addAttribute(new DistanceAttribute(distance));
+			}
+
+			track.addTrackpoint(trackpoint);
+			
+			// get ready for next iteration
+			lastElevation = trackpoint.getAttribute(AltitudeAttribute.class).getAltitude();
+			lastPosition = trackpoint.getPosition();
+		}
+		activity.addTrack(track);
+		return activity;
+	}
+
+	private Date parseTime(Element trk) {
+		String timeText = trk.getChildTextTrim("time", NS_GPX_1_1);
+		if (timeText != null && timeText.length() > 0) {
+			return parseTime(timeText);
+		}
+		return new Date();
+	}
+
+	private Date parseTime(String timeText) {
+		try {
+			// 2012-01-01T15:45:47Z
+			return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(timeText);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private double getElevationForPosition(double latitude, double longitude) {
---
+++ com.github.rasifix.trainings.format.gpx/src/main/test/com/github/rasifix/trainings/format/gpx/internal/GpxReaderTest.java
@@ -0,0 +1,55 @@
+package com.github.rasifix.trainings.format.gpx.internal;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.rasifix.trainings.model.Activity;
+import com.github.rasifix.trainings.model.Track;
+import com.github.rasifix.trainings.model.Trackpoint;
+import com.github.rasifix.trainings.model.attr.AltitudeAttribute;
+
+public class GpxReaderTest {
+	
+	@Test
+	public void testReadGpxTrack() throws Exception {
+		GpxReader reader = new GpxReader();
+		InputStream inputStream = getClass().getResourceAsStream("/com/github/rasifix/trainings/format/gpx/internal/2012-01-01-1445.gpx");
+		List<Activity> activities = reader.readActivities(inputStream);
+		
+		assertEquals(1, activities.size());
+		
+		Activity activity = activities.get(0);
+		assertEquals(date("2012-01-01 15:45:47"), activity.getStartTime());
+		assertEquals(1, activity.getTrackCount());
+		
+		Track track = activity.getTrack(0);
+		assertEquals(date("2012-01-01 15:45:47"), track.getStartTime());
+		assertEquals(703, track.getTrackpointCount());
+		assertEquals(8325, track.getDistance());
+		assertEquals(47 * 60 + 49, track.getDuration());
+		
+		assertTrackpoint(0, 47.015954000, 7.455408000, 563.0, track, 0);
+		assertTrackpoint(73 * 1000, 47.016458000, 7.454054000, 561.6, track, 10);
+		assertTrackpoint((47 * 60 + 49) * 1000, 47.016537000, 7.453624000, 562.0, track, 702);
+	}
+
+	private void assertTrackpoint(int elapsedTime, double lat, double lon, double ele, Track track, int trackpointIndex) {
+		Trackpoint trackpoint = track.getTrackpoint(trackpointIndex);
+		assertEquals("elapsedTime", elapsedTime, trackpoint.getElapsedTime());
+		assertEquals("lat", lat, trackpoint.getPosition().getLatitude(), 1e-9);
+		assertEquals("lon", lon, trackpoint.getPosition().getLongitude(), 1e-9);
+		assertEquals("ele", ele, trackpoint.getAttribute(AltitudeAttribute.class).getAltitude(), 1e-9);
+	}
+
+	private Date date(String date) throws ParseException {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
+	}
+	
+}
